bf4553f5b1c74dacaf437227fdb43227ddf3f666
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2023-04-27 19:23:20 -0300

    consertado

--- scripts.js
+++ scripts.js
@@ -1,22 +1,26 @@
 const amongus = document.getElementById('amongus');
 const zedamanga = document.getElementById('zedamanga');
 
+const musica1 = document.getElementById('audio1');
+const musica2 = document.getElementById('audio2');
+const musica3 = document.getElementById('audio3');
+
 amongus.addEventListener('mouseenter', () => {
-    audio1.play();
+    musica1.play();
 })
 amongus.addEventListener('mouseout', () => {
-    audio1.pause();
-    audio1.currentTime = 0;
+    musica1.pause();
+    musica1.currentTime = 0;
 })
 amongus.addEventListener('mousedown', () => {
-    audio1.pause();
-    audio2.currentTime = 0;
-    audio2.play();
+    musica1.pause();
+    musica2.currentTime = 0;
+    musica2.play();
 })
 amongus.addEventListener('mouseup', () => {
-    audio1.play();
+    musica1.play();
 })
 zedamanga.addEventListener('mouseenter', () => {
-    audio3.currentTime = 0;
-    audio3.play();
+    musica3.currentTime = 0;
+    musica3.play();
 })
